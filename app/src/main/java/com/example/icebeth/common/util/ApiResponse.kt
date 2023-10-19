175a49f2aa677ff9a2e48298886546f7d72c420c
Nejer <Nejer6@gmail.com>
2023-10-20 00:42:21 +0300

    add: ApiResponse documentation

--- app/src/main/java/com/example/icebeth/common/util/ApiResponse.kt
+++ app/src/main/java/com/example/icebeth/common/util/ApiResponse.kt
@@ -2,12 +2,33 @@ package com.example.icebeth.common.util
 
 import io.ktor.http.HttpStatusCode
 
+/**
+ * Represents the result of an API response, which can be a success or an error.
+ *
+ * @param T The type of data in the response body.
+ */
 sealed class ApiResponse<out T> {
+    /**
+     * Represents a successful API response with a response body.
+     *
+     * @property body The response body.
+     */
     data class Success<T>(val body: T): ApiResponse<T>()
 
+    /**
+     * Represents an error response, which can be an HTTP error or a network error.
+     */
     sealed class Error : ApiResponse<Nothing>() {
+        /**
+         * Represents an HTTP error response with the HTTP status code.
+         *
+         * @property status The HTTP status code associated with the error.
+         */
         data class Http(val status: HttpStatusCode) : Error()
 
+        /**
+         * Represents a network error response.
+         */
         object Network : Error()
     }
 }

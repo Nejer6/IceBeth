ff641d31a35122c389e0af39100119d1cf5f4bb0
Nejer <Nejer6@gmail.com>
2023-10-29 18:25:08 +0300

    add: location PermissionDialog

--- app/src/main/java/com/example/icebeth/MainActivity.kt
+++ app/src/main/java/com/example/icebeth/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.icebeth
 import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Lifecycle
+import com.example.icebeth.common.presentation.components.LocationPermissionTextProvider
+import com.example.icebeth.common.presentation.components.OnLifecycleEvent
+import com.example.icebeth.common.presentation.components.PermissionDialog
 import com.example.icebeth.common.presentation.theme.IceBethTheme
+import com.example.icebeth.common.util.hasLocationPermission
+import com.example.icebeth.common.util.openAppSettings
 import com.example.icebeth.core.presentation.navigation.AppNavigation
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,23 +31,65 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ),
-            0
-        )
-
         setContent {
             IceBethTheme {
-                // A surface container using the 'background' color from the theme
+                var isFineLocationPermissionGranted by remember {
+                    mutableStateOf(false)
+                }
+                var isCoarseLocationPermissionGranted by remember {
+                    mutableStateOf(false)
+                }
+
+                val locationPermissionResultLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestMultiplePermissions(),
+                    onResult = { perms ->
+                        isFineLocationPermissionGranted =
+                            perms[Manifest.permission.ACCESS_FINE_LOCATION] == true
+                        isCoarseLocationPermissionGranted =
+                            perms[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+
+                    }
+                )
+
+                LaunchedEffect(key1 = isCoarseLocationPermissionGranted) {
+                    locationPermissionResultLauncher.launch(
+                        arrayOf(
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        )
+                    )
+                }
+
+                OnLifecycleEvent { owner, event ->
+                    when (event) {
+                        Lifecycle.Event.ON_RESUME -> {
+                            isFineLocationPermissionGranted = this.hasLocationPermission()
+                        }
+
+                        else -> {}
+                    }
+                }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
                     AppNavigation()
+
+                    if (!isFineLocationPermissionGranted)
+                        PermissionDialog(
+                            permissionTextProvider = LocationPermissionTextProvider(),
+                            isPermanentlyDeclined = !shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION),
+                            onDismiss = { },
+                            onOkClick = {
+                                locationPermissionResultLauncher.launch(
+                                    arrayOf(
+                                        Manifest.permission.ACCESS_FINE_LOCATION
+                                    )
+                                )
+                            },
+                            onGoToAppSettingsClick = ::openAppSettings
+                        )
                 }
             }
         }

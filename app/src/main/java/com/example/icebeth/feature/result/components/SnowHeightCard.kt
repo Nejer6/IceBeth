c7ae0daaef08dca1e476b81b51fe3ca93d761d68
Nejer <Nejer6@gmail.com>
2023-11-10 13:54:19 +0300

    add: CardWithColumn

--- app/src/main/java/com/example/icebeth/feature/result/components/SnowHeightCard.kt
+++ app/src/main/java/com/example/icebeth/feature/result/components/SnowHeightCard.kt
@@ -1,46 +1,21 @@
 package com.example.icebeth.feature.result.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.icebeth.common.presentation.theme.spacing
 import com.example.icebeth.feature.result.ResultState
 
 @Composable
-fun SnowHeightCard(state: ResultState) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(MaterialTheme.spacing.medium)
-                .fillMaxWidth()
-        ) {
-            SplitText(
-                text1 = "Средняя высота снега: ",
-                text2 = String.format("%.1f", state.result.averageSnowHeight),
-                Modifier.fillMaxWidth()
-            )
-            SplitText(
-                text1 = "Минимальная высота снега: ",
-                text2 = state.result.minSnowHeight.toString(),
-                Modifier.fillMaxWidth()
-            )
-            SplitText(
-                text1 = "Максимальная высота снега: ",
-                text2 = state.result.maxSnowHeight.toString(),
-                Modifier.fillMaxWidth()
-            )
-            SplitText(
-                text1 = "Сумма высот снега: ",
-                text2 = state.result.sumOfSnowHeights.toString(),
-                Modifier.fillMaxWidth()
-            )
-        }
-    }
+fun SnowHeightCard(
+    state: ResultState,
+    modifier: Modifier = Modifier
+) {
+    CardWithColumn(
+        modifier = modifier,
+        items = listOf(
+            "Средняя высота снега: " to String.format("%.1f", state.result.averageSnowHeight),
+            "Минимальная высота снега: " to state.result.minSnowHeight.toString(),
+            "Максимальная высота снега: " to state.result.maxSnowHeight.toString(),
+            "Сумма высот снега: " to state.result.sumOfSnowHeights.toString()
+        )
+    )
 }
--- app/src/main/java/com/example/icebeth/feature/result/components/SnowHeightCountCard.kt
+++ app/src/main/java/com/example/icebeth/feature/result/components/SnowHeightCountCard.kt
@@ -1,42 +1,20 @@
 package com.example.icebeth.feature.result.components
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.icebeth.common.presentation.theme.spacing
 import com.example.icebeth.feature.result.ResultState
 
 @Composable
-fun SnowHeightCountCard(state: ResultState) {
-    Card(
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .padding(MaterialTheme.spacing.medium)
-        ) {
-            SplitText(
-                text1 = "Высот снега от 1 до 3:",
-                text2 = state.result.sum13.toString(),
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            SplitText(
-                text1 = "Высот снега от 4 до 6:",
-                text2 = state.result.sum46.toString(),
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            SplitText(
-                text1 = "Высот снега долее 30:",
-                text2 = state.result.heightGreaterThan30.toString(),
-                modifier = Modifier.fillMaxWidth()
-            )
-        }
-    }
+fun SnowHeightCountCard(
+    state: ResultState,
+    modifier: Modifier = Modifier
+) {
+    CardWithColumn(
+        modifier = modifier,
+        items = listOf(
+            "Высот снега от 1 до 3: " to state.result.sum13.toString(),
+            "Высот снега от 4 до 6: " to state.result.sum46.toString(),
+            "Высот снега долее 30: " to state.result.heightGreaterThan30.toString()
+        )
+    )
 }
